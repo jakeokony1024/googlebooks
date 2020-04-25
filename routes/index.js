4d9cd0325401593523639a0c369ba05463a5e042
Jakeokony1024 <jakeokony@gmail.com>
2020-04-24 19:42:21 -0500

    set up basic routing and exports

--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,9 @@ const router = require("express").Router();
 const apiRoutes = require("./API");
 
 router.use ("./API", apiRoutes);
+
+router.use(function(req, res){
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
+
+module.exports = router;
