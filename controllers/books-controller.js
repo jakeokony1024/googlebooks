727cd565a23d491a51da55f124739c314d731ac4
Jakeokony1024 <jakeokony@gmail.com>
2020-04-26 12:31:51 -0500

    fixed bookescontroller.js added routes to server.js

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -2,28 +2,28 @@ const db = require("../models");
 
 module.exports = {
 	findAll: function (req, res) {
-		db.Item.find(req.query)
+		db.Book.find(req.query)
 			.sort({ date: -1 })
 			.then((dbModel) => res.json(dbModel))
 			.catch((err) => res.json(err));
 	},
 	findById: function (req, res) {
-		db.Item.find({ _id: req.params.id })
+		db.Book.find({ _id: req.params.id })
 			.then((dbModel) => res.json(dbModel))
 			.catch((err) => res.json(err));
 	},
 	create: function (req, res) {
-		db.Item.create(req.body)
+		db.Book.create(req.body)
 			.then((dbModel) => res.json(dbModel))
 			.catch((err) => res.json(err));
 	},
 	update: function (req, res) {
-		db.Item.findOneAndUpdate({ _id: req.params.id }, req.body)
+		db.Book.findOneAndUpdate({ _id: req.params.id }, req.body)
 			.then((dbModel) => res.json(dbModel))
 			.catch((err) => res.json(err));
 	},
 	remove: function (req, res) {
-		db.Item.findById({ _id: req.params.id })
+		db.Book.findById({ _id: req.params.id })
 			.then((dbModel) => dbModel.remove())
 			.then((dbModel) => res.json(dbModel))
 			.catch((err) => res.json(err));
