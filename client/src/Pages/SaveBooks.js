18d917dfbf6cca9b02f4d434c360a14225f58595
Jakeokony1024 <jakeokony@gmail.com>
2020-04-26 15:30:19 -0500

    fixed err in savebooks by correcting spelling

--- client/src/Pages/SaveBooks.js
+++ client/src/Pages/SaveBooks.js
@@ -1,38 +1,40 @@
 import React, { Component } from "react";
-import { Container } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
 import API from "../Utils/API";
-import SavedResult from "../components/SavedResult";
+import Jumbotron from "../components/Jumbotron";
+import { Container} from "../components/Grid";
+import SavedResult from "../components/SavedResult"
 
-class SaveBooks extends Component {
-	state = {
-		savedBooks: []
-	};
+class SaveBook extends Component {
+    state = {
+        savedBooks: []
+    };
 
-	componentDidMount() {
-		API.getBook()
-			.then(res.setState( {savedBooks: res.data }))
-			.catch(err => console.log(err))
-	}
+    //when this component mounts, grab all books that were save to the database 
+    componentDidMount() {
+        API.getBooks()
+            .then(res => this.setState({ savedBooks: res.data }))
+            .catch(err => console.log(err))
+    }
 
-	handleDeleteButton = id => {
-		API.deleteBook(id) 
-			.then(res => this.componentDidMount())
-			.catch(err => console.log(err))
-	}
+    //function to remove book by id
+    handleDeleteButton = id => {
+        API.deleteBook(id)
+            .then(res => this.componentDidMount())
+            .catch(err => console.log(err))
+    }
 
-	render() {
-		return (
-			<Container fluid className = "container">
-				<Jumbotron/>
-				<Container>
-					<SavedResult 
-					savedBooks = {this.state.savedBooks} 
-					handleDeleteButton = {this.handleDeleteButton}/>
-				</Container>
-			</Container>
-		)
-	}
+    render() {
+        return (
+            <Container fluid className="container">
+                <Jumbotron />
+                <Container>
+                    <SavedResult savedBooks={this.state.savedBooks} handleDeleteButton={this.handleDeleteButton} />
+                </Container>
+            </Container>
+        )
+    }
 }
 
-export default SaveBooks;
+
+
+export default SaveBook;
--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -1,23 +1,24 @@
 import axios from "axios";
 
 export default {
-	// Gets all items
-	getGoogleSearchBooks: function () {
+	// Gets all books from google search
+	getGoogleSearchBooks: function (query) {
 		return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
 	},
-	// Gets the item with the given id
+	//get all books from db
+	getBooks: function() {
+		return axios.get("api/books");
+	},
+	// Gets the books with the given id
 	getBook: function (id) {
 		return axios.get("/api/books/" + id);
 	},
-	updateBook: function (id, bookData) {
-		return axios.put("/api/books/" + id, bookData);
-	},
-	// Deletes the item with the given id
+	// Deletes the books with the given id
 	deleteBook: function (id) {
 		return axios.delete("/api/books/" + id);
 	},
-	// Saves an item to the database
-	saveBook: function (bookData) {
-		return axios.post("/api/books", bookData);
+	// Saves an books to the database
+	saveBook: function (savedBooks) {
+		return axios.post("/api/books", savedBooks);
 	},
 };
